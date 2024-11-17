b0ba3f4594d1de4822fab1d47a7c0a5d0ae6a49e
MaiCVCR <mairen.chavarria.v@gmail.com>
2024-11-17 01:35:56 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import OnboardingWizard from './components/TelegramHealthCheckIn'
+
+function App() {
+
+  return (
+    <>
+    <OnboardingWizard />
+    </>
+  )
+}
+
+export default App
